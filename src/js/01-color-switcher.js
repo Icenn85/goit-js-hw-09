878b79298cd0b6efd082b8ecf7cae203bd5da857
Anastasiia Yavtushenko <Icenn85@gmail.com>
2022-08-27 15:13:20 +0300

    add task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+btnStart = document.querySelector('button[data-start]');
+btnStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+btnStop.disabled = true;
+
+
+btnStart.addEventListener('click', onStartChangeColor);
+btnStop.addEventListener('click', onStopChangeColor);
+
+function onStartChangeColor() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopChangeColor() {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(timerId);
+}
